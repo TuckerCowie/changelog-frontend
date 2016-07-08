54177f8aaeb88afe0d89fa1df4df25adaf848db2
Tucker Cowie <tuckercowie@gmail.com>
2016-07-08 10:09:46 -0500

    Add font assets, font fallbacks and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,30 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var pug = require('gulp-pug');
 var imagemin = require('gulp-imagemin');
+var fontmin = require('gulp-fontmin');
 
 var sources = {
 	views: ['src/views/**/*.pug', '!src/views/{partials,templates}/*.pug'],
 	pug: ['src/views/**/*.pug'],
 	sass: 'src/sass/**/*.sass',
 	images: 'assets/img/**/*.{svg,png,jpg}',
+	fonts: 'assets/fonts/**/*.ttf',
 };
 
 var dist = 'dist';
 
-gulp.task('default', ['sass', 'views', 'images']);
+gulp.task('default', ['sass', 'views', 'images', 'fonts']);
 
 gulp.task('clean', function cleanDist() {
-	del(dist);
+	del(dist + '/*.*');
+});
+
+gulp.task('clean:images', function cleanDistImages() {
+	del(dist + '/img');
+});
+
+gulp.task('clean:fonts', function cleanDistFonts() {
+	del(dist + '/fonts');
 });
 
 gulp.task('watch', ['clean', 'default'], function liveReload() {
@@ -48,8 +58,14 @@ gulp.task('sass', function buildCSS() {
 	.pipe(livereload());
 });
 
-gulp.task('images', function compressImages() {
+gulp.task('images', ['clean:images'], function compressImages() {
 	gulp.src(sources.images)
 	.pipe(imagemin())
 	.pipe(gulp.dest(dist + '/img'));
+});
+
+gulp.task('fonts', ['clean:fonts'], function compressFonts() {
+	gulp.src(sources.fonts)
+	.pipe(fontmin())
+	.pipe(gulp.dest(dist + '/fonts'));
 });
